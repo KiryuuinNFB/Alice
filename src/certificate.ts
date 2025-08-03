026099836b03908c4da9c6a62ebb6cfbd2cca354
KiryuuinNFB <doraemonstichisnoon@gmail.com>
2025-08-03 18:11:11 +0700

    simple certificate generator with token

---
+++ src/certificate.ts
@@ -0,0 +1,84 @@
+import { Elysia, status, t, file } from "elysia";
+const { SVG, registerWindow } = require('@svgdotjs/svg.js')
+const { createSVGWindow } = require('svgdom')
+const sharp = require('sharp')
+import { jwt } from '@elysiajs/jwt'
+import { PrismaClient } from "@prisma/client";
+import fs from 'fs'
+
+interface JwtPayload {
+    username: string
+}
+
+const prisma = new PrismaClient()
+
+const prefixHandle = (prefix: string | undefined) => {
+    switch (prefix) {
+        case "DekChai":
+            return "ด.ช."
+        case "DekYing":
+            return "ด.ญ."
+        case "NangSao":
+            return "นางสาว"
+        case "Nang":
+            return "นาง"
+        case "Nai":
+            return "นาย"
+        default:
+            return "อื่นๆ"
+    }
+}
+
+export const certificate = new Elysia({ prefix: '/cert' })
+    .use(
+        jwt({
+            name: 'jwt',
+            secret: "CHANGE_THIS_IN_PROD"
+        })
+    )
+    .get("/", async ({ jwt, set, headers, status }) => {
+        set.headers["Content-Type"] = "image/png";
+        const auth = headers.authorization
+        if (!auth) {
+            return status(401, "Unauthorized")
+        }
+        const decoded = await jwt.verify(auth) as unknown as JwtPayload
+
+        const decodeduser = await prisma.user.findUnique({
+            where: {
+                username: decoded?.username!
+            }
+        });
+
+        const switchPrefix = prefixHandle(decodeduser?.prefix)
+
+        const window = createSVGWindow()
+        const document = window.document
+        registerWindow(window, document)
+        const draw = SVG(document.documentElement).size(800, 600)
+        draw.rect(800, 600).fill('#fdf6e3')
+
+
+        draw.image('./elysialmao.jpeg')
+            .size(800, 600)
+            .move(0, 0)
+            
+        draw.text("เกลียดติบัดร")
+            .move(400, 180)
+            .font({ size: 28, weight: 'bold', anchor: 'middle' })
+
+        draw.text(`${switchPrefix} ${decodeduser?.name} ${decodeduser?.surname}`)
+            .move(400, 250)
+            .font({ size: 28, weight: 'bold', anchor: 'middle' })
+
+        draw.text("ได้เข้าร่วมวันวิทยาสาด")
+            .move(400, 300)
+            .font({ size: 28, weight: 'bold', anchor: 'middle' })
+
+        const svgString = draw.svg()
+        const pngBuffer = await sharp(Buffer.from(svgString)).png().toBuffer()
+        return pngBuffer
+
+    })
+
+
